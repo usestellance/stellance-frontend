60a6af55660d8cc456d82504afac2354204c6784
Tubolayefa <tlayefaah@gmail.com>
2025-06-18 00:08:00 +0100

    feat: implement CreateInvoice component with layout and styling; update Page component structure

--- src/app/(app)/(dashboard)/invoice/page.tsx
+++ src/app/(app)/(dashboard)/invoice/page.tsx
@@ -1,12 +1,37 @@
+import Image from "next/image";
 import React from "react";
+import AppButton from "../../../../components/webapp/ui/AppButton";
 
 const CreateInvoice = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-[163px] lg:w-[200px] mx-auto mt-[80px] lg:mt-[50px]">
+        <Image
+          alt="Invoice Illustration"
+          src="/images/new_Invoice.svg"
+          className="h-full w-full object-contain"
+          height={500}
+          width={500}
+        />
+      </div>
+      <p className="max-w-[294px] lg:max-w-[482px] mx-auto text-center lg:text-xl font-bold mt-4 lg:mt-6">
+        You have not created any invoice yet. <br /> All created invoices would
+        be displayed here.
+      </p>
+      <AppButton
+        size="lg"
+        href="#"
+        className="w-full mt-7 lg:mt-8 max-w-[320px] sm:max-w-[400px] lg:max-w-[490px]"
+      >
+        Create Invoice
+      </AppButton>
+    </div>
+  );
 };
 
 export default function Page() {
   return (
-    <div>
+    <div className="myContainer">
       <CreateInvoice />
     </div>
   );
