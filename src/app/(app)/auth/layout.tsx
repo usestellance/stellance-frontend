4459ca6b758983e8d7767797a6f244cb3c36f248
Tubolayefa <tlayefaah@gmail.com>
2025-06-17 23:24:23 +0100

    feat: simplify Logo component by removing height props and adjusting styles

--- src/app/(app)/auth/layout.tsx
+++ src/app/(app)/auth/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
 }>) {
   return (
     <div className="px-5 pt-7 pb-16 lg:pb-[60px] max-w-[1800px] mx-auto relative min-h-screen">
-      <Logo height="h-[34px] md:h-[80px]" />
+      <Logo />
       {/* <PageLoading showLogo /> */}
       <div className="mt-10 relative max-w-[620px] mx-auto">{children}</div>
     </div>
