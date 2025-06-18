ba968161236cc2607a5cdff8c1efe735930e676f
Tubolayefa <tlayefaah@gmail.com>
2025-06-18 18:12:23 +0100

    fix: correct border property syntax and adjust width settings for invoice number display

--- src/app/(app)/(dashboard)/invoice/create/page.tsx
+++ src/app/(app)/(dashboard)/invoice/create/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
           <div className="bg-[#D9E4F8] rounded-[3px] text-text-strong h-[30px] w-[25px] md:h-[40px] md:w-[43px] lg:h-[60px] lg:w-[63px]  flex items-center justify-center text-sm font-medium lg:text-xl">
             #
           </div>
-          <div className=" border border-[#AAAAAA] font-medium text-xs h-[30px] w-full flex items-center px-[10px] py-[6px] rounded-[6px] max-w-[113px] md:h-[40px] lg:max-w-[248px] lg:h-[60px] lg:text-base lg:px-[22px]">
+          <div className=" border border[#AAAAAA] font-medium text-xs h-[30px] flex items-center px-[10px] py-[6px] rounded-[6px] min-w-[100px] md:h-[40px] lg:min-w-[150px] bg-[#D9E4F8] lg:h-[60px] lg:text-base lg:px-[22px]">
             INV-001
           </div>
         </div>
