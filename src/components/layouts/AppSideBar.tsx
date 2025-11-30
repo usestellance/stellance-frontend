ae9d58abe3a8e20166515ea66929c9789d0f9a9e
Tubolayefa <tlayefaah@gmail.com>
2025-11-30 01:55:47 +0100

    Refactor dashboard and invoices pages; reintroduce layout and loading components
    - Deleted old dashboard, create invoice, invoices, loading, notifications, profile, and wallet pages.
    - Added new dashboard structure with layout and loading components.
    - Implemented AppSidebar for navigation with updated routes.
    - Introduced new UI components including Separator, Sheet, Skeleton, and Tooltip.
    - Created a responsive sidebar with mobile support using hooks.

---
+++ src/components/layouts/AppSideBar.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar, // Import this hook
+} from "@/components/ui/sidebar";
+import Logo from "../shared/Logo";
+import { dashboardRoutes, invoiceRoutes } from "../../config/constants/routes";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: dashboardRoutes.HOME,
+    icon: Home,
+  },
+  {
+    title: "Invoices",
+    url: invoiceRoutes.INVOICES,
+    icon: Inbox,
+  },
+  {
+    title: "Wallet",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Receipt",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Sign out",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  const pathname = usePathname();
+  const { setOpenMobile, isMobile } = useSidebar(); // Get sidebar controls
+
+  const handleLinkClick = () => {
+    if (isMobile) {
+      setOpenMobile(false); // Close sidebar on mobile
+    }
+  };
+
+  return (
+    <Sidebar className="bg-neutral-500 px-2 rounded-tr-[20px] rounded-br-[20px]">
+      <SidebarHeader className="bg-neutral-500">
+        <div className="flex justify-center my-10">
+          <Logo />
+        </div>
+      </SidebarHeader>
+      <SidebarContent className="bg-neutral-500">
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton isActive={pathname.includes(item.url)} asChild>
+                <Link
+                  href={item.url}
+                  onClick={handleLinkClick} // Add click handler
+                  className="h-[43px] flex justify-center items-center rounded-md transition-all data-[active=true]:bg-primary-50 data-[active=true]:text-primary-600 data-[active=true]:font-bold text-[20px]"
+                >
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
