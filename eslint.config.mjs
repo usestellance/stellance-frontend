e9b7d1869e4542881bf754adef0c95126006ade9
Tubolayefa <tlayefaah@gmail.com>
2025-12-08 11:05:28 +0100

    added patch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import reactHooks from 'eslint-plugin-react-hooks';
-import { defineConfig } from 'eslint/config';
 
 
 const eslintConfig = defineConfig([
