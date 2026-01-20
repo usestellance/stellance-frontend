2d196f636e2c61847ed653efe05d7b6e8f8e354d
Tubolayefa <tlayefaah@gmail.com>
2026-01-20 16:33:32 +0100

    push 2

--- src/app/(webapp)/(dashboard)/receipts/page.tsx
+++ src/app/(webapp)/(dashboard)/receipts/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-import React from "react";
-import Receipts from "../../../../features/receipt/components/Receipts";
+// "use client";
+// import React from "react";
+// import Receipts from "../../../../features/receipt/components/Receipts";
 
-export default function Page() {
-  return (
-    <div className="pt-5 pb-20">
-      <div className="custom-container">
-        <h2 className="h2-app">Receipts</h2>
+// export default function Page() {
+//   return (
+//     <div className="pt-5 pb-20">
+//       <div className="custom-container">
+//         <h2 className="h2-app">Receipts</h2>
 
-        <section className="lg:mt-[30px]">
-          <Receipts />
-        </section>
-      </div>
-    </div>
-  );
-}
+//         <section className="lg:mt-[30px]">
+//           <Receipts />
+//         </section>
+//       </div>
+//     </div>
+//   );
+// }
