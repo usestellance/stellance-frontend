82f83ec543a11375d098f67357accc89e9b88082
Tubolayefa <tlayefaah@gmail.com>
2026-02-19 11:57:27 +0100

    adjusted width

--- src/components/landing/YourWork.tsx
+++ src/components/landing/YourWork.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const YourWork = () => {
   return (
     <section className="py-[60px] md:py-24 lg:py-40">
-      <div className="landing-container sm:flex sm:items-center gap-10 md:gap-[60px]">
+      <div className="landing-container sm:flex sm:items-center gap-10 md:gap-[60px] sm:justify-between">
         <div className="sm:w-1/2">
           <h2 className="text-[32px] font-bold text-center sm:text-start lg:text-[48px] leading-[120%]">
             Your work, your worth, accessed fairly and securely.
@@ -16,13 +16,13 @@ const YourWork = () => {
             conversion.
           </p>
         </div>
-        <div className="w-full mx-auto mt-[30px] sm:w-1/2">
+        <div className="w-full mx-auto mt-[30px] sm:w-1/2 max-w-[601px]">
           <Image
             alt="Guy on a laptop with a dog"
             height={500}
             width={500}
             src="/images/your_work.png"
-            className="w-full h-full object-contain"
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
